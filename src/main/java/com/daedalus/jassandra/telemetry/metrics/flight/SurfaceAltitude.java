294321a9c7484230c197aaec16c7779e867eb4ea
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2019-08-01 20:58:36 +1000

    Added hashable to surface altitude

--- src/main/java/com/daedalus/jassandra/telemetry/metrics/flight/SurfaceAltitude.java
+++ src/main/java/com/daedalus/jassandra/telemetry/metrics/flight/SurfaceAltitude.java
@@ -1,13 +1,25 @@
 package com.daedalus.jassandra.telemetry.metrics.flight;
 
+import com.daedalus.jassandra.telemetry.metrics.IHashable;
 import com.daedalus.jassandra.telemetry.metrics.IMetric;
 import krpc.client.RPCException;
 import krpc.client.services.SpaceCenter;
 
-public class SurfaceAltitude implements IMetric {
+import java.util.HashMap;
+
+public class SurfaceAltitude implements IMetric, IHashable {
     @Override
     public Double get(SpaceCenter.Vessel vessel, SpaceCenter.ReferenceFrame referenceFrame)
             throws RPCException {
         return vessel.flight(referenceFrame).getSurfaceAltitude();
+    }
+
+    @Override
+    public HashMap getHashMap(SpaceCenter.Vessel vessel, SpaceCenter.ReferenceFrame referenceFrame) throws RPCException {
+        HashMap<String, Double> flightSurfaceAltitude = new HashMap<>();
+        flightSurfaceAltitude.put(this.getClass().getName(),
+                vessel.flight(referenceFrame).getSurfaceAltitude());
+
+        return flightSurfaceAltitude;
     }
 }
