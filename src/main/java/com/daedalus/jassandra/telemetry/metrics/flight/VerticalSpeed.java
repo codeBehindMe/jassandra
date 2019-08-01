39e2f8deb9e55b8c6151a40af5329eefc36016df
Aaron Tillekeratne <aaron.tillekeratne@gmail.com>
2019-08-01 21:08:30 +1000

    Added hashable to vertical speed

--- src/main/java/com/daedalus/jassandra/telemetry/metrics/flight/VerticalSpeed.java
+++ src/main/java/com/daedalus/jassandra/telemetry/metrics/flight/VerticalSpeed.java
@@ -1,13 +1,23 @@
 package com.daedalus.jassandra.telemetry.metrics.flight;
 
+import com.daedalus.jassandra.telemetry.metrics.IHashable;
 import com.daedalus.jassandra.telemetry.metrics.IMetric;
 import krpc.client.RPCException;
 import krpc.client.services.SpaceCenter;
 
-public class VerticalSpeed implements IMetric {
+import java.util.HashMap;
+
+public class VerticalSpeed implements IMetric, IHashable {
     @Override
     public Double get(SpaceCenter.Vessel vessel, SpaceCenter.ReferenceFrame referenceFrame)
             throws RPCException {
         return vessel.flight(referenceFrame).getVerticalSpeed();
+    }
+
+    @Override
+    public HashMap getHashMap(SpaceCenter.Vessel vessel, SpaceCenter.ReferenceFrame referenceFrame) throws RPCException {
+        HashMap<String, Double> verticalSpeed = new HashMap<String, Double>();
+        verticalSpeed.put(this.getClass().getName(), vessel.flight(referenceFrame).getVerticalSpeed());
+        return verticalSpeed;
     }
 }
